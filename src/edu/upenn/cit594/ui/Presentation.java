7e7cb634dbad24eeaf197c9ea75fb65e6ea67039
dmlarkin <daveylarkin@gmail.com>
2019-12-02 15:47:46 -0700

    Quick Update

--- src/edu/upenn/cit594/ui/Presentation.java
+++ src/edu/upenn/cit594/ui/Presentation.java
@@ -1,7 +1,9 @@
 package edu.upenn.cit594.ui;
 
 import edu.upenn.cit594.processor.ParkingProcessor;
+import sun.security.ssl.Debug;
 import edu.upenn.cit594.logging.Logger;
+import java.util.Scanner;
 
 public class Presentation {
 
@@ -16,6 +18,14 @@ public class Presentation {
 		Logger logger = Logger.getInstance();
 				
 //		System.out.println("Please check " + logger.getFilename() + " for the results!");
+		
+		// Create User Interaction Interface //
+		
+//		System.out.println("Which action would you like to perform (0-6)?:");
+//		Scanner s = new Scanner(System.in);
+//		System.out.println(s.nextLine());
+		
+		
 		
 	}
 	
